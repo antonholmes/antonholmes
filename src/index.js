7a75cce6fa2c8382e211bc03bd25e32a964d1fa0
Anton Holmes <holmesanton@icloud.com>
2018-11-21 18:04:27 -0500

    feat(init): other initialized files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createBrowserHistory } from 'history'
-import { Router, Route, Switch } from 'react-router'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router';
 
 // import indexRoutes from 'routes/index.jsx'
 
 // import 'assets/scss/material-kit-react.css?v=1.1.0'
 
-var hist = createBrowserHistory()
+var hist = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={hist} onUpdate={() => window.scrollTo(0, 0)}>
     <Switch>
-      {indexRoutes.map((prop,key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
       })}
     </Switch>
-  </Router>
+  </Router>,
   document.getElementById('root')
-)
+);
